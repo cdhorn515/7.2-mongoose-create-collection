7f652f63f968ac90c5cc9a8a7dfc6b18709e4594
Christina Cornell <cdhorn515@gmail.com>
2017-07-06 15:56:24 -0400

    can add pig to database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mustacheExpress = require('mustache-express');
 var bodyParser = require('body-parser');
 var qs = require('qs');
+var path = require('path');
 var mongoose = require('mongoose');
 var pigController = require('./controllers/pigController');
 
@@ -13,17 +14,21 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', './views');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/cdccollection');
 
-app.get('/', pigController.list);
+app.get('/', pigController.indexLanding);
+app.post('/', pigController.indexList);
 
-// Pig.findOne({name: 'watering can'}).then(function(result){
+// Pig.findOneAndUpdate({name: 'watering can'},[{ownerName: 'Christina', howLongOwned: 6}, {ownerName: 'Peter', howLongOwned: 2}]).then(function(error, result){
 //   //can save results in context obj and send to mustache file
 //   console.log(result);
-//   var owners = [{ownerName: 'Christina', howLongOwned: 6}, {ownerName: 'Peter', howLongOwned: 2}];
-//   result.owners.push(owners);
-//   result.save();
 // });
 app.listen(3000, function(){
   console.log('app started');
--- controllers/pigController.js
+++ controllers/pigController.js
@@ -1,11 +1,27 @@
 var Pig = require('../models/pig');
 
 module.exports = {
-  list: function (req, res) {
-    console.log('hi');
+  indexLanding: function (req, res) {
     Pig.find().then(function(results){
       res.render('pig/list', {model:results});
     });
+  },
 
+
+  indexList: function (req, res) {
+    console.log('hi');
+
+      Pig.create({name: req.body.name});
+      var pig = new Pig({name: req.body.name, size: req.body.size, color: req.body.color, owners: [{ownerName: req.body.owner, howLongOwned: req.body.yearsOwned}, {ownerName: req.body.owner, howLongOwned: req.body.yearsOwned}], attributes: [req.body.attributes], howMany: req.body.howMany});
+      pig.save().then(function(){
+        console.log('saving new pig');
+      }).catch(function(error){
+        console.log('error ', error);
+      });
+
+      Pig.find().then(function(results){
+        console.log('should add new pig');
+      res.render('pig/list', {model:results});
+    });
   }
 };
